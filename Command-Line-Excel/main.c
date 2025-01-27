6e7223c930117238afc723e8d8809acf3b35e0d4
evans-prince <princeevans2004@gmail.com>
2025-01-27 12:59:47 +0530

    Initial Commit

---
+++ Command-Line-Excel/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Command-Line-Excel
+//
+//  Created by PRINCE  on 1/27/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
