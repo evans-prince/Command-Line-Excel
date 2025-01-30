ef4025c33656ef662536f707908b589e051f3698
Cyanide-03 <aditya.ag0301@gmail.com>
2025-01-30 07:58:15 +0000

    Add: Display functionality

--- Command-Line-Excel/src/display.c
+++ Command-Line-Excel/src/display.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define MIN(a,b) (a<b?a:b);
 #define MAX(a,b) (a<b?b:a);
@@ -19,14 +20,18 @@ void display_sheet(sheet *s){
     int last_col=MIN(first_col+9,cols-1);
 
     int width=10;
-    printf("\t\t");
+
+    // Print the column names
+    printf("\t");
     for(int j=first_col;j<=last_col;j++){
         printf("%-*s\t",width,col_index_to_name(j));
     }
 
+    // s->grid[3][3].val=INT_MAX;
+    // Print the grid
     printf("\n");
     for (int i=first_row;i<=last_row;i++){
-        printf("%d\t\t",i+1);
+        printf("%d\t",i+1);
         for(int j=first_col;j<=last_col;j++){
             printf("%-*d\t",width,s->grid[i][j].val);
         }
@@ -35,7 +40,7 @@ void display_sheet(sheet *s){
 }
 
 // int main(){
-//     sheet *s=create_sheet(5,5);
+//     sheet *s=create_sheet(55,55);
 //     display_sheet(s);
 //     return 0;
 // }
