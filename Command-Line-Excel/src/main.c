3109dd051a7940cd402099be0e4a0c7f5acd1673
evans-prince <princeevans2004@gmail.com>
2025-02-02 20:26:39 +0530

    Added infinite loop functionality.

--- Command-Line-Excel/src/main.c
+++ Command-Line-Excel/src/main.c
@@ -6,17 +6,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-int main(void) {
-    // insert code here...
-    int r,c;
-    scanf("Enter row : %d Enter col : %d ", &r , &c);
-    sheet *s = create_sheet(r,c);
-    //printf("%d , %d \n" , s->bounds.first_row , s->bounds.first_col );
+// insert code here...
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: ./sheet <num1> <num2>\n");
+        return 1;
+    }
+    
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    sheet *s = create_sheet(num1,num2);
     display_sheet(s);
-    //scroll(s, 'd');
-    //scroll(s, 'w');
-    //printf("%d , %d \n" , s->bounds.first_row , s->bounds.first_col );
-
+    char *input = NULL;
+    size_t len = 0;
+    char command;
+    
+    while (true) {
+        printf("Enter the command (or 'q' to quit): ");
+        getline(&input, &len, stdin);
+        
+        // Remove newline character
+        input[strcspn(input, "\n")] = 0;
+        
+        // Exit if user enters 'q'
+        if (strcmp(input, "q") == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+        
+        // Parse input and call function
+        if (sscanf(input, "%s" ,&command) == 1) {
+            scroll(s, command);
+            display_sheet(s);
+        } else {
+            printf("Invalid input! Try again.\n");
+        }
+    }
+    
+    free(input);
+    
     return 0;
 }
